129d210104f1980e2d669344c8799f447a19c808
AOE-Dylan <dylantt2000@gmail.com>
2016-11-23 09:01:22 -1000

    100% plezzzzzzzz halppppppppppp meeeeeeeee

--- test_review.js
+++ test_review.js
@@ -1,11 +1,11 @@
-var knight = "Lacelot";
-var maiden = "Jane";
+var knight = "Arthur";
+var maiden = "Guinivere";
 var monster = "Dragon";
 var weaponChest = ["Mace", "Sorceror's Stone", "Excalibur"];
 var hero = {
   wizard : "Merlin",
   weapon : "Mace",
-  power : 21
+  power : 9000
 };
 
 function swordInTheStone(worthy) {
@@ -20,21 +20,21 @@ swordInTheStone(knight)
 
 function trueLove(king, queen) {
   if (queen == "Guinivere" && king == "Arthur") {
-    console.log("On this day, our " + king + " and " + queen + " shall rule happily ever after.") 
+    console.log("On this day, our " + king + " and " + queen + " shall rule happily ever after.");
   } else {
-    console.log("We must keep searching, for true love must reign over Camelot.")
+    console.log("We must keep searching, for true love must reign over Camelot.");
   }
 }
 trueLove(knight, maiden)
 
 function epicBattle(wizard, enemy, power) {
   if (power <= 15) {
-    console.log("Our" + wizard + " has fallen.")
-  } else if (power >= 16 && power <= 40){
-    console.log(wizard + " has almost slain " + enemy)
-  } else {
-    console.log("Our " + wizard + " has slain the " + enemy)
+    console.log("Our " + wizard + " has fallen.");
+  }else if(power >= 16 && power <= 40){
+    console.log(wizard + " has almost slain " + enemy);
+  }else{
+    console.log("Our " + wizard + " has slain the " + enemy);
   }
 }
 
-epicBattle(weaponChest[0], monster, weaponChest[2])
+epicBattle(hero.wizard, monster, hero.power)
